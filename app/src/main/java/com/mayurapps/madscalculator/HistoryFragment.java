bac014ab95d065ebe6406d18ec6db6a5e76b9128
Mayur Patil <mayurpatil307@gmail.com>
2020-08-21 00:16:50 +0530

    add files and MADS rule logic

---
+++ app/src/main/java/com/mayurapps/madscalculator/HistoryFragment.java
@@ -0,0 +1,70 @@
+package com.mayurapps.madscalculator;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class HistoryFragment extends Fragment {
+
+    private Button backButton;
+    private ListView historyListView;
+    SharedPreferences sharedPreferences;
+    //HashSet<String> opSet;
+
+    public HistoryFragment() {
+        // Required empty public constructor
+    }
+
+    public static HistoryFragment newInstance() {
+        HistoryFragment fragment = new HistoryFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //sharedPreferences = getActivity().getSharedPreferences("com.mayurapps.madscalculator", Context.MODE_PRIVATE);
+        //opSet = (HashSet<String>) sharedPreferences.getStringSet("ops", null);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        Bundle bundle=getArguments();
+
+        ArrayList<String> opList = new ArrayList<>();
+        opList = bundle.getStringArrayList("operations");
+
+        View view = inflater.inflate(R.layout.fragment_history, container, false);
+        historyListView = view.findViewById(R.id.history_list_view);
+        backButton = view.findViewById(R.id.back_button_fragment);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1, opList);
+        historyListView.setAdapter(adapter);
+
+        return view;
+    }
+
+
+}
--- app/src/main/java/com/mayurapps/madscalculator/MainActivity.java
+++ app/src/main/java/com/mayurapps/madscalculator/MainActivity.java
@@ -1,14 +1,183 @@
 package com.mayurapps.madscalculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    // IDs of all the numeric buttons
+    private int[] numericButtons = {R.id.btn_zero, R.id.btn_one, R.id.btn_two, R.id.btn_three, R.id.btn_four, R.id.btn_five, R.id.btn_six, R.id.btn_seven, R.id.btn_eight, R.id.btn_nine};
+    // IDs of all the operator buttons
+    private int[] operatorButtons = {R.id.btn_add, R.id.btn_subtract, R.id.btn_multiply, R.id.btn_divide};
+    // TextView used to display the output
+    private TextView txtScreen;
+    // Represent whether the lastly pressed key is numeric or not
+    private boolean lastNumeric;
+    // Represent that current state is in error or not
+    private boolean stateError;
+    // If true, do not allow to add another DOT
+    private boolean lastDot;
+
+    ArrayList<String> operations;
+
+    private FrameLayout fragmentContainer;
+    FragmentManager fragmentManager;
+    private Button historyBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        operations = new ArrayList<>();
+
+        // Find the TextView
+        this.txtScreen = (TextView) findViewById(R.id.txtScreen);
+        // Find and set OnClickListener to numeric buttons
+        setNumericOnClickListener();
+        // Find and set OnClickListener to operator buttons, equal button and decimal point button
+        setOperatorOnClickListener();
+
+        fragmentContainer = (FrameLayout) findViewById(R.id.fragment_container);
+        historyBtn = (Button) findViewById(R.id.historyBtn);
+        historyBtn.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("CommitPrefEdits")
+            @Override
+            public void onClick(View v) {
+                openFragment();
+            }
+        });
     }
+
+    public void openFragment() {
+        HistoryFragment historyFragment = HistoryFragment.newInstance();
+        Bundle args = new Bundle();
+        args.putStringArrayList("operations", operations);
+        historyFragment.setArguments(args);
+
+        fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_right, R.anim.enter_from_right, R.anim.exit_to_right);
+        transaction.addToBackStack(null);
+        transaction.add(R.id.fragment_container, historyFragment, "HISTORY_FRAGMENT").commit();
+    }
+
+    /**
+     * Find and set OnClickListener to numeric buttons.
+     */
+    private void setNumericOnClickListener() {
+        // Create a common OnClickListener
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Just append/set the text of clicked button
+                Button button = (Button) v;
+                if (stateError) {
+                    // If current state is Error, replace the error message
+                    txtScreen.setText(button.getText());
+                    stateError = false;
+                } else {
+                    // If not, already there is a valid expression so append to it
+                    txtScreen.append(button.getText());
+                }
+                // Set the flag
+                lastNumeric = true;
+            }
+        };
+        // Assign the listener to all the numeric buttons
+        for (int id : numericButtons) {
+            findViewById(id).setOnClickListener(listener);
+        }
+    }
+
+    /**
+     * Find and set OnClickListener to operator buttons, equal button and decimal point button.
+     */
+    private void setOperatorOnClickListener() {
+        // Create a common OnClickListener for operators
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // If the current state is Error do not append the operator
+                // If the last input is number only, append the operator
+                if (lastNumeric && !stateError) {
+                    Button button = (Button) v;
+                    txtScreen.append(button.getText());
+                    lastNumeric = false;
+                    lastDot = false;    // Reset the DOT flag
+                }
+            }
+        };
+        // Assign the listener to all the operator buttons
+        for (int id : operatorButtons) {
+            findViewById(id).setOnClickListener(listener);
+        }
+        // Decimal point
+        findViewById(R.id.btn_dot).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (lastNumeric && !stateError && !lastDot) {
+                    txtScreen.append(".");
+                    lastNumeric = false;
+                    lastDot = true;
+                }
+            }
+        });
+        // Clear button
+        findViewById(R.id.btnClear).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtScreen.setText("");  // Clear the screen
+                // Reset all the states and flags
+                lastNumeric = false;
+                stateError = false;
+                lastDot = false;
+            }
+        });
+        // Equal button
+        findViewById(R.id.btn_equal).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onEqual();
+            }
+        });
+    }
+
+    /**
+     * Logic to calculate the solution.
+     */
+    private void onEqual() {
+        double result = 0;
+        // If the current state is error, nothing to do.
+        // If the last input is a number only, solution can be found.
+        if (lastNumeric && !stateError) {
+            // Read the expression
+            String txt = txtScreen.getText().toString();
+            try {
+                // Calculate the result and display
+                result = EvaluationUtils.evaluate(txt);
+                txtScreen.setText(Double.toString(result));
+                lastDot = true; // Result contains a dot
+            } catch (ArithmeticException ex) {
+                // Display an error message
+                txtScreen.setText("Error");
+                stateError = true;
+                lastNumeric = false;
+            }
+
+            operations.add(txt+" = "+ String.valueOf(result));
+        }
+    }
+
 }
